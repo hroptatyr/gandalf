71d194bfd05faa3d435832ea5b227d57adf6e392
Sebastian Freundt <freundt@ga-group.nl>
2011-09-05 13:56:47 +0000

    minor, build gandqry tool and provide a stress test for the server
    Signed-off-by: Sebastian Freundt <freundt@ga-group.nl>

--- cli/gandqry.c
+++ cli/gandqry.c
@@ -5,10 +5,16 @@
 /* our stuff */
 #include "gandapi.h"
 
+#if !defined UNUSED
+# define UNUSED(_x)	_x __attribute__((unused))
+#endif	/* !UNUSED */
+
+static size_t npkt = 0;
+
 static int
-cb(gand_res_t res, void *clo)
+cb(gand_res_t UNUSED(res), void *UNUSED(clo))
 {
-	fputs("packet\n", stderr);
+	npkt++;
 	return 0;
 }
 
@@ -18,9 +24,14 @@ main(int argc, const char *argv[])
 	gand_ctx_t g;
 	const char *vf[] = {{"open"}, {"high"}, {"low"}, {"close"}};
 
-	g = gand_open("chantico:8623", 2000);
-	gand_get_series(g, "USD@ecb", vf, 0, cb, NULL);
-	gand_close(g);
+	for (size_t i = 0; i < 100; i++) {
+		npkt = 0;
+		g = gand_open("gonzo:8624", 2000);
+		gand_get_series(g, "988006@comd", vf, 4, cb, NULL);
+		gand_close(g);
+		fprintf(stdout, "number of packets %zu\n", npkt);
+	}
+
 	return 0;
 }
 
