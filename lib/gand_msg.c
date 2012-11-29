65e9408a94f0fd77fc3aac316dc9241de9403cbd
Sebastian Freundt <freundt@ga-group.nl>
2012-11-29 12:05:54 +0000

    minor, parse simple URL cases (/info?... and /series?...)
    Signed-off-by: Sebastian Freundt <freundt@ga-group.nl>

--- lib/gand_msg.c
+++ lib/gand_msg.c
@@ -14,7 +14,70 @@ static size_t msg_pidx = 0;
 static int
 __parse_http(gand_msg_t msg, const char *req, size_t rsz)
 {
-	return -1;
+/* we assume we get the GET / bit
+ * we can parse:
+ * GET /series?sym=foo  or GET /series?rid=14
+ * GET /info?sym=bar  or GET /info?rid=13 
+ *
+ * get_series "NYMEX_HRC_201011@cme" --select sym,d,vf,v -i --filter stl
+ * becomes
+ * sym=NYMEX_HRC_201011@cme&select=sym,d,vf,v&ignore-case&filter=stl */
+	static const char svc_ser[] = "/series?";
+	static const size_t svz_ser = sizeof(svc_ser) - 1U;
+	static const char svc_nfo[] = "/info?";
+	static const size_t svz_nfo = sizeof(svc_nfo) - 1U;
+	const char *eol;
+	const char *svc;
+	const char *arg;
+
+	if (UNLIKELY((eol = memchr(req, '\n', rsz)) == NULL)) {
+		/* weirdo message, don't bother */
+		return -1;
+	}
+
+	/* consider the range [req + 4, eol) and look for services */
+	if ((svc = memmem(req, eol - req, svc_ser, svz_ser)) != NULL) {
+		gand_set_msg_type(msg, GAND_MSG_GET_SER);
+		arg = svc + svz_ser;
+	} else if ((svc = memmem(req, eol - req, svc_nfo, svz_nfo)) != NULL) {
+		gand_set_msg_type(msg, GAND_MSG_GET_NFO);
+		arg = svc + svz_nfo;
+	} else {
+		/* don't worry about it */
+		return -1;
+	}
+
+	/* we want ?sym=XYZ or ?rid=NNN */
+	{
+		static const char arg_rid[] = "rid=";
+		static const size_t arz_rid = sizeof(arg_rid) - 1U;
+		static const char arg_sym[] = "sym=";
+		static const size_t arz_sym = sizeof(arg_sym) - 1U;
+
+#define R(p)	(eol - (p))
+		/* look for sym='s */
+		for (const char *p = arg, *eoa;
+		     (p = memmem(p, R(p), arg_sym, arz_sym)) != NULL &&
+			     ((eoa = memchr(p, '&', R(p))) != NULL ||
+			      (eoa = memchr(p, ' ', R(p))) != NULL);
+		     p = eoa + 1) {
+			resize_rolf_objs(msg);
+			msg->rolf_objs[msg->nrolf_objs++].rolf_sym =
+				strndup(p + arz_sym, eoa - (p + arz_sym));
+		}
+		/* and again, for rid= */
+		for (const char *p = arg, *eoa;
+		     (p = memmem(p, R(p), arg_rid, arz_rid)) != NULL &&
+			     ((eoa = memchr(p, '&', R(p))) != NULL ||
+			      (eoa = memchr(p, ' ', R(p))) != NULL);
+		     p = eoa) {
+			resize_rolf_objs(msg);
+			msg->rolf_objs[msg->nrolf_objs++].rolf_id =
+				strtoul(p + arz_rid, NULL, 10);
+		}
+#undef R
+	}
+	return 0;
 }
 
 
@@ -90,7 +153,7 @@ gand_parse_blob(gand_ctx_t *ctx, const char *buf, size_t bsz)
 
 	if ((p = memmem(buf, bsz, "GET ", 4)) != NULL) {
 		/* http request */
-		rc = __parse_http(res, p + 4, bsz - (p - buf) - 4);
+		rc = __parse_http(res, p, bsz - (p - buf));
 	} else {
 		/* could be classic gandalf command syntax */
 		rc = __parse(res, buf, bsz);
