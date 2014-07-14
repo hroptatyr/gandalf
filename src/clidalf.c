d404c6139ac39e215c3662716e3088168a4cf443
Sebastian Freundt <freundt@ga-group.nl>
2014-07-14 09:13:19 +0000

    minor, show actual results upon clidalf show, filtering is still missing

--- src/clidalf.c
+++ src/clidalf.c
@@ -13,6 +13,18 @@
 #include "nifty.h"
 #include "fops.h"
 
+typedef struct {
+	const char *s;
+	size_t z;
+} word_t;
+
+struct rln_s {
+	word_t sym;
+	word_t dat;
+	word_t vrb;
+	word_t val;
+};
+
 static char *trolfdir = "/var/scratch/freundt/trolf";
 static size_t trolfdiz = sizeof("/var/scratch/freundt/trolf") - 1U/*\nul*/;
 static dict_t gsymdb;
@@ -59,6 +71,118 @@ make_lateglu_name(dict_oid_t rolf_id)
 	return f;
 }
 
+static struct rln_s
+snarf_rln(const char *ln, size_t lz)
+{
+	struct rln_s r;
+	const char *p;
+
+	/* normally first up is the rolf-id, overread him */
+	if (UNLIKELY((p = memchr(ln, '\t', lz)) == NULL)) {
+		goto b0rk;
+	}
+
+	/* snarf sym */
+	r.sym.s = ++p;
+	/* find separator between symbol and trans-id */
+	if (UNLIKELY((p = memchr(p, '\t', ln + lz - p)) == NULL)) {
+		goto b0rk;
+	}
+	r.sym.z = p++ - r.sym.s;
+
+	/* find separator between trans-id and date stamp */
+	if (UNLIKELY((p = memchr(p, '\t', ln + lz - p)) == NULL)) {
+		goto b0rk;
+	}
+
+	/* snarf date */
+	r.dat.s = ++p;
+	/* find separator between date stamp and valflav aka verb */
+	if (UNLIKELY((p = memchr(p, '\t', ln + lz - p)) == NULL)) {
+		goto b0rk;
+	}
+	r.dat.z = p++ - r.dat.s;
+
+	/* snarf valflav aka verb */
+	r.vrb.s = p;
+	/* find separator between date stamp and valflav aka verb */
+	if (UNLIKELY((p = memchr(p, '\t', ln + lz - p)) == NULL)) {
+		goto b0rk;
+	}
+	r.vrb.z = p++ - r.vrb.s;
+
+	/* snarf value */
+	r.val.s = p;
+	r.val.z = ln + lz - p;
+
+	/* that's all */
+	return r;
+
+b0rk:
+	return (struct rln_s){NULL};
+}
+
+static void
+filtshow(const char *data, const size_t dlen)
+{
+	char buf[4096U];
+	size_t tot = 0U;
+
+	/* go through the buffer generate snarf lines */
+	for (size_t i = 0U; i < dlen; i++) {
+		const char *const bol = data + i;
+		const size_t max = dlen - i;
+		char *restrict bp = buf + tot;
+		const char *eol;
+		struct rln_s ln;
+
+		if (UNLIKELY((eol = memchr(bol, '\n', max)) == NULL)) {
+			eol = bol + max;
+		}
+		/* snarf the line, v0 format, zero copy */
+		ln = snarf_rln(bol, eol - bol);
+
+	filt:
+		/* apply filters */
+		;
+
+		if (UNLIKELY(ln.sym.s == NULL)) {
+			continue;
+		} else if (UNLIKELY(tot +
+				    ln.sym.z + 1U/*\t*/ +
+				    ln.dat.z + 1U/*\t*/ +
+				    ln.vrb.z + 1U/*\t*/ +
+				    ln.val.z + 1U/*\n*/ >= sizeof(buf))) {
+			/* flush */
+			write(STDOUT_FILENO, buf, tot);
+			bp = buf + (tot = 0U);
+		}
+
+		/* show results */
+		memcpy(bp, ln.sym.s, ln.sym.z);
+		bp += ln.sym.z;
+		*bp++ = '\t';
+
+		memcpy(bp, ln.dat.s, ln.dat.z);
+		bp += ln.dat.z;
+		*bp++ = '\t';
+
+		memcpy(bp, ln.vrb.s, ln.vrb.z);
+		bp += ln.vrb.z;
+		*bp++ = '\t';
+
+		memcpy(bp, ln.val.s, ln.val.z);
+		bp += ln.val.z;
+		*bp++ = '\n';
+
+		tot += bp - (buf + tot);
+		i += eol - bol;
+	}
+	/* flush again */
+	write(STDOUT_FILENO, buf, tot);
+	return;
+}
+
 
 #include "clidalf.yucc"
 
@@ -86,11 +210,8 @@ Error: cannot access lateglu file: %s  (%08u)\n", sym, rid);
 			continue;
 		}
 
-		/* apply filters */
-		;
-
-		/* show results */
-		;
+		/* filter and show results */
+		filtshow(fb.fb.d, fb.fb.z);
 
 		/* and close the bugger again */
 		munmap_fn(fb);
