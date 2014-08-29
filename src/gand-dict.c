cf3830471ad60c47b7d9e4bcc11972ce8f07c73e
Sebastian Freundt <freundt@ga-group.nl>
2014-08-29 18:10:33 +0000

    minor, quick'n'dirty migrate dict API to redland

--- src/gand-dict.c
+++ src/gand-dict.c
@@ -37,15 +37,37 @@
 #if defined HAVE_CONFIG_H
 # include "config.h"
 #endif	/* HAVE_CONFIG_H */
+#include <stdlib.h>
+#include <string.h>
 #include <fcntl.h>
-#include <tcbdb.h>
+#define USE_REDLAND
+#if defined USE_REDLAND
+# include <redland.h>
+#else
+# include <tcbdb.h>
+#endif
 #include "gand-dict.h"
 #include "nifty.h"
+
+#if defined USE_REDLAND
+static librdf_world *wrld;
+static librdf_storage *stor;
+#endif	/* USE_REDLAND */
 
 
 dict_t
 open_dict(const char *fn, int oflags)
 {
+#if defined USE_REDLAND
+	(void)oflags;
+
+	if (wrld == NULL) {
+		wrld = librdf_new_world();
+	}
+	stor = librdf_new_storage(
+		wrld, "hashes", fn, "hash-type='bdb',dir='.'");
+	return librdf_new_model(wrld, stor, NULL);
+#else
 	int omode = BDBOREADER;
 	dict_t res;
 
@@ -72,19 +94,62 @@ free_out:
 	tcbdbdel(res);
 out:
 	return NULL;
+#endif
 }
 
 void
 close_dict(dict_t d)
 {
+#if defined USE_REDLAND
+	librdf_free_model(d);
+	librdf_free_storage(stor);
+	librdf_free_world(wrld);
+	wrld = NULL;
+#else
 	tcbdbclose(d);
 	tcbdbdel(d);
+#endif	/* USE_REDLAND */
 	return;
 }
 
 dict_oid_t
 dict_sym2oid(dict_t d, const char sym[static 1U], size_t ssz)
 {
+#if defined USE_REDLAND
+	static const unsigned char _s[] = "http://lakshmi:8080/v0/series/";
+	static const unsigned char _v[] = "http://www.ga-group.nl/rolf/1.0/rid";
+	static unsigned char *rs;
+	static size_t rz;
+	librdf_node *s, *p;
+	dict_oid_t rid = 0;
+
+	if (UNLIKELY(rs == NULL)) {
+		rz = sizeof(_s) + ssz + 64U;
+		rz &= ~(64U - 1U);
+		rs = malloc(rz);
+		/* prefix with _s */
+		memcpy(rs, _s, sizeof(_s));
+	} else if (UNLIKELY(sizeof(_s) + ssz > rz)) {
+		rz = sizeof(_s) + ssz + 64U;
+		rz &= ~(64U - 1U);
+		rs = realloc(rs, rz);
+	}
+	memcpy(rs + sizeof(_s) - 1U, sym, ssz);
+	rs[sizeof(_s) + ssz - 1U] = '\0';
+
+	s = librdf_new_node_from_uri_string(wrld, rs);
+	p = librdf_new_node_from_uri_string(wrld, _v);
+
+	with (librdf_node *res = librdf_model_get_target(d, s, p)) {
+		if (LIKELY(res != NULL)) {
+			const unsigned char *val =
+				librdf_node_get_literal_value(res);
+			rid = strtoul((const char*)val, NULL, 10);
+		}
+	}
+	return rid;
+
+#else
 	const dict_oid_t *rp;
 	int rz[1];
 
@@ -94,6 +159,7 @@ dict_sym2oid(dict_t d, const char sym[static 1U], size_t ssz)
 		return 0U;
 	}
 	return *rp;
+#endif
 }
 
 /* gand-dict.c ends here */
