8ad3efba46db4cbf9d5b3ceb8ae7c77d062695fd
Sebastian Freundt <freundt@ga-group.nl>
2014-09-01 14:53:58 +0000

    cosmetics, produce log output without additional newlines

--- src/gandalfd.c
+++ src/gandalfd.c
@@ -743,7 +743,7 @@ work_ser(gand_httpd_req_t req)
 		};
 	}
 
-	GAND_INFO_LOG("  %s -> %08u\n", sym, rid);
+	GAND_INFO_LOG("  %s -> %08u", sym, rid);
 	/* otherwise we've got some real yacka to do */
 	if (UNLIKELY((fn = make_lateglu_name(rid)) == NULL)) {
 		goto interr;
