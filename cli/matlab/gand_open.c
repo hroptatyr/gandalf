d1971035a610e87f3194c173ade14248391c14a7
Sebastian Freundt <freundt@ga-group.nl>
2014-09-04 14:48:19 +0000

    chore, also allow mex.h coming from octave/mex.h

--- cli/matlab/gand_open.c
+++ cli/matlab/gand_open.c
@@ -39,7 +39,11 @@
 #endif	/* HAVE_CONFIG_H */
 #include <stdint.h>
 /* matlab stuff */
-#include <mex.h>
+#if defined HAVE_OCTAVE_MEX_H
+# include <octave/mex.h>
+#else  /* !HAVE_OCTAVE_MEX_H */
+# include <mex.h>
+#endif	/* HAVE_OCTAVE_MEX_H */
 /* our stuff */
 #include "gandapi.h"
 #include "gand_handle.h"
